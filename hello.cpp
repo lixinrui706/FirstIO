f355dd96d083b6c047499f6bacf5bdfd561ad765
lixinrui706 <xli21@mail.csuchico.edu>
2018-01-29 17:39:54 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// This is a single-line comment because it follows the two forward-slashes.Author:Xinrui li
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi,my name is Xinrui" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
